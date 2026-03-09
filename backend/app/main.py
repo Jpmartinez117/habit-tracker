bc13247df175ee547a11e6b430a5ad962a7703a4
Jpmartinez117 <jpmartinez117@gmail.com>
2026-03-09 19:39:16 -0400

    CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .core.database import engine
 
-app = FastAPI()
+app = FastAPI(
+    title = "Habit Tracker API",
+    description = "API for managing habits and tracking progress",
+    version = "1.0.0",
+    docs_url = "/docs",
+    redoc_url = "/redoc",
+)
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
